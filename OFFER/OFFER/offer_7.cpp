6655b39daf9f6b37741ca56978ab929c6e59df66
longmao-yiran <594652170@qq.com>
2018-02-21 10:46:42 +0800

    add oofer_7 test and code in book

--- OFFER/OFFER/offer_7.cpp
+++ OFFER/OFFER/offer_7.cpp
@@ -1,4 +1,5 @@
 #include "offer_7.h"
+#include "Utilities\BinaryTree.h"
 
 /*
 	输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
@@ -7,18 +8,11 @@
 	则重建二叉树并返回。
 */
 
-struct BinaryTreeNode
-{
-	int m_nValuel;
-	BinaryTreeNode* m_pLeft;
-	BinaryTreeNode* m_pRight;
-};
-
 BinaryTreeNode* ConstructCore(int* stratPreoder, int* endPreoder, int *stratInorder, int* endInorder)
 {
 	int rootValue = *stratPreoder;
 	BinaryTreeNode* root = new BinaryTreeNode();
-	root->m_nValuel = rootValue;
+	root->m_nValue = rootValue;
 	root->m_pLeft = nullptr;
 	root->m_pRight = nullptr;
 
@@ -56,7 +50,7 @@ BinaryTreeNode* Construct(int* perorder, int*inorder, int length)
 void PreorderTraverse(BinaryTreeNode* tree) 
 {
 	if (tree != nullptr) {
-		cout << tree->m_nValuel << "\t";
+		cout << tree->m_nValue << "\t";
 		PreorderTraverse(tree->m_pLeft);
 		PreorderTraverse(tree->m_pRight);
 	}
@@ -116,13 +110,144 @@ void PreorderTraverse(TreeNode* tree)
 	}
 }
 
+// ====================测试代码====================
+void Test(char* testName, int* preorder, int* inorder, int length)
+{
+	if (testName != nullptr)
+		printf("%s begins:\n", testName);
+
+	printf("The preorder sequence is: ");
+	for (int i = 0; i < length; ++i)
+		printf("%d ", preorder[i]);
+	printf("\n");
+
+	printf("The inorder sequence is: ");
+	for (int i = 0; i < length; ++i)
+		printf("%d ", inorder[i]);
+	printf("\n");
+
+	try
+	{
+		BinaryTreeNode* root = Construct(preorder, inorder, length);
+		PrintTree(root);
+
+		DestroyTree(root);
+	}
+	catch (std::exception& exception)
+	{
+		printf("Invalid Input.\n");
+	}
+}
+
+// 普通二叉树
+//              1
+//           /     \
+//          2       3  
+//         /       / \
+//        4       5   6
+//         \         /
+//          7       8
+void Test1()
+{
+	const int length = 8;
+	int preorder[length] = { 1, 2, 4, 7, 3, 5, 6, 8 };
+	int inorder[length] = { 4, 7, 2, 1, 5, 3, 8, 6 };
+
+	Test("Test1", preorder, inorder, length);
+}
+
+// 所有结点都没有右子结点
+//            1
+//           / 
+//          2   
+//         / 
+//        3 
+//       /
+//      4
+//     /
+//    5
+void Test2()
+{
+	const int length = 5;
+	int preorder[length] = { 1, 2, 3, 4, 5 };
+	int inorder[length] = { 5, 4, 3, 2, 1 };
+
+	Test("Test2", preorder, inorder, length);
+}
+
+// 所有结点都没有左子结点
+//            1
+//             \ 
+//              2   
+//               \ 
+//                3 
+//                 \
+//                  4
+//                   \
+//                    5
+void Test3()
+{
+	const int length = 5;
+	int preorder[length] = { 1, 2, 3, 4, 5 };
+	int inorder[length] = { 1, 2, 3, 4, 5 };
+
+	Test("Test3", preorder, inorder, length);
+}
+
+// 树中只有一个结点
+void Test4()
+{
+	const int length = 1;
+	int preorder[length] = { 1 };
+	int inorder[length] = { 1 };
+
+	Test("Test4", preorder, inorder, length);
+}
+
+// 完全二叉树
+//              1
+//           /     \
+//          2       3  
+//         / \     / \
+//        4   5   6   7
+void Test5()
+{
+	const int length = 7;
+	int preorder[length] = { 1, 2, 4, 5, 3, 6, 7 };
+	int inorder[length] = { 4, 2, 5, 1, 6, 3, 7 };
+
+	Test("Test5", preorder, inorder, length);
+}
+
+// 输入空指针
+void Test6()
+{
+	Test("Test6", nullptr, nullptr, 0);
+}
+
+// 输入的两个序列不匹配
+void Test7()
+{
+	const int length = 7;
+	int preorder[length] = { 1, 2, 4, 5, 3, 6, 7 };
+	int inorder[length] = { 4, 2, 8, 1, 6, 3, 7 };
+
+	Test("Test7: for unmatched input", preorder, inorder, length);
+}
+
+
 void offer_7_test()
 {
-	int predoer[9] = { 1,2,4,7,3,5,6,8 };
-	int inorder[9] = { 4,7,2,1,5,3,8,6 };
-	BinaryTreeNode* root = Construct(predoer, inorder, 8);
- 	PreorderTraverse(root);
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	Test6();
+	Test7();
 
+	int predoer[9] = { 1,2,4,7,3,5,6,8 };
+	int inorder[9] = { 4,7,2,1,5,3,8,6 }; 
 	// 牛客网vector接口使用方法
 	vector<int> predoerVector(&predoer[0], &predoer[8]);
 	vector<int> inorderVector(&inorder[0], &inorder[8]);
